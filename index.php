63af56b4aeeebfbaecd71f64fa9c485f037e9174
MigyA <migyaxel@gmail.com>
2018-01-15 22:18:21 +0100

    test

--- index.php
+++ index.php
@@ -1,39 +1,4 @@
-<?php
 
-// Controleur Principal du site Forma 2017
-
-session_start();
-require_once('util/fonctions.inc.php');
-require_once("util/class.pdoForma.inc.php");
-include("vues/v_entete.php");
-include("vues/v_bandeau.php");
-
-$dateActuelle = date('Y-m-d ');
-
-echo " La salade  verte ";
-$_SESSION['DateActuelle'] = $dateActuelle;
-if(isset($_SESSION['message']))
-{
-    include('vues/v_erreurs.php');
-    unset($_SESSION['message']);
-}
-
-
-
-if(!isset($_REQUEST['Chemin']))
-{
-    $Chemin = 'accueil';
-}
-else
-{
-    $Chemin = $_REQUEST['Chemin'];
-
-/* Création de l'instance d'accès à la base de données */
-}
-$pdo = PdoForma::getPdoForma();
-
-
-switch($Chemin)
 {
     case 'accueil' :
     {include("vues/v_accueil.php");break;}
